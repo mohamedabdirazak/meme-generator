18950da65044e9e40939724915306bdcf53b9bbf
mohamed madobe <mohamedmadobe@mohameds-MacBook-Pro.local>
2023-01-13 21:54:36 -0600

    test

---
+++ app.js
@@ -0,0 +1,16 @@
+const shopping = [
+    {
+bradcode: 23,
+price : 12
+
+    },
+ {
+    bradcode: 22,
+price : 24, 
+
+
+ }
+]
+const shoppingList = shopping.map(function(shopping){
+    return shopping.price
+})
